f470fbfba39dd287eb982a4a404754cb554d2fdb
joeyzazalin <josephanteyi@gmail.com>
2024-06-06 16:51:06 -0700

    binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,7 +8,7 @@
  * Return: pointer to the new node - NULL on failure
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
